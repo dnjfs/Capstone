3b990c9067244c7ce8586b235a7adc561dd5a891
DESKTOP-MLQVVMG\good <whwnsgk94@naver.com>
2021-05-14 01:54:06 +0900

    애니메이션 추가 플레이어 이미지 고양이에서 크레인으로 교체

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
+
+
 {
+    Animator animator;
     public int a = 0;
     public int goal = 3;
-    void Update()
+
+    void start()
     {
-        Vector3 MousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
-        MousePosition.z = 0f;
-        this.transform.position = MousePosition;
+        this.animator = GetComponent<Animator>();
+        this.animator.speed = 0;
+       
+
     }
+
+    public void Update()
+    {
+
+        
+            
+            
+                Vector3 MousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+                MousePosition.z = 0f;
+                this.transform.position = MousePosition;
+                
+            
+       
+    }
+
+   
+
+
+
     /*
     Rigidbody2D rigid2D;
     private Vector3 m_Offset;
@@ -41,6 +65,8 @@ public class PlayerController : MonoBehaviour
     */
 
     void OnCollisionEnter2D(Collision2D coll)
+
+
     {
         
         if (coll.gameObject.tag == "Doll")
@@ -79,6 +105,7 @@ public class PlayerController : MonoBehaviour
 
             }
 
+            
 
            
         }
